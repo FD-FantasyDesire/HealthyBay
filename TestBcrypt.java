86710379fc26b6b5b07d2f3a449e044b90648613
TennouboshiUzume <104900248+FD-FantasyDesire@users.noreply.github.com>
2026-03-22 15:22:43 +0800

    init update

--- TestBcrypt.java
+++
@@ -1,2 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;  
-public class TestBcrypt { public static void main(String[] args) { System.out.println(new BCryptPasswordEncoder().encode("123456")); } }  
